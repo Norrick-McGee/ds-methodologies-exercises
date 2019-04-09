eca80423fce6da8c1bc3570c41fb7d443619cd3c
Norrick <Norrick.mcgee@gmail.com>
2019-04-09 09:41:05 -0500

    mall acq and prep finished

--- clustering/prepare_mall.py
+++ clustering/prepare_mall.py
@@ -1,9 +1,28 @@
 from env import user,password,host
-
-def detect_outliers(df):
-    ""
+from sklearn.preprocessing import LabelEncoder
+import pandas as pd
 
 def encode_gender(df):
     encoder = LabelEncoder()
     encoder.fit(df.gender)
     return df.assign(gender = encoder.transform(df.gender))
+
+def detect_outliers(df, col='annual_income'):
+    q3 = df[col].quantile(q=.75)
+    q1 = df[col].quantile(q=.25)
+    iqr = q3 - q1
+    iqr_scaled = iqr*1.5
+    outlier_floor = iqr_scaled - q1
+    outlier_ciel = iqr_scaled + q3
+    outlier_df = pd.concat([df[df[col] > outlier_ciel],df[df[col] < outlier_floor]])
+    return outlier_df
+
+def drop_outliers(df):
+    outliers = detect_outliers(df)
+    dfc = df.copy()
+    for i in outliers.index:
+        dfc.drop(i,inplace=True)
+    return dfc
+
+def prep_mall(df):
+    return df.pipe(encode_gender).pipe(drop_outliers)
