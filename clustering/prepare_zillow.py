dd94aad546d7d0e6ba355e61f7509ac3339c8fcb
Norrick <Norrick.mcgee@gmail.com>
2019-04-12 14:53:42 -0500

    new methods added to prepare, and more exploration done

--- clustering/prepare_zillow.py
+++ clustering/prepare_zillow.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+from scipy.stats import iqr
 
 def unit_count_impute(df):
     df = df.copy()
@@ -13,7 +15,7 @@ def drop_no_bathbr(df):
 def drop_dup_parcelid(df):
     return df.drop_duplicates(subset='parcelid')
 
-def df_missing_cols(df):
+def df_missing_values_in_cols(df):
     cols = list(df.columns)
     percent_missing = []
     for col in cols:
@@ -24,16 +26,67 @@ def df_missing_cols(df):
 
     return pd.DataFrame(list(zip(cols, percent_missing, tot_missing)), columns=['column','percent missing','total missing'])
 
-def df_missing_v_rows(df):
-    ### Returns the isnull count for columns for each row as a Series (can be added to DF)
-    return df.T.isnull().sum()
+
+def df_missing_cols_in_rows(df):
+    df = df.copy()
+    if 'missing' in list(df.columns):
+        df.drop('missing',axis=1,inplace=True)
+    missing_col_by_row = df.T.isnull().sum()
+    total_cols = df.shape[1]
+    missing_cols_percentage = missing_col_by_row/total_cols
+    df['missing'] = missing_col_by_row
+    df['missing_percentage'] = missing_cols_percentage
+    return df
+
+def missing_values(df,rows=False):
+    if rows:
+        return df_missing_cols_in_rows(df)
+    else:
+        return df_missing_values_in_cols(df)
+
+def drop_cols_missing_percentage(df,percentage):
+    columns_missing_more_than_50 = list(missing_values(df)[missing_values(df)['percent missing'] > percentage].column)
+    return df.drop(columns_missing_more_than_50,axis=1)
 
 def drop_multiunits(df):
     return df.drop(df[df.unitcnt>1].index)
+
+def drop_missing_rows_by_column(df,col):
+    return df[~df[col].isnull()]
+
+def outliers(df,col,drop_top=False,drop_bot=False):
+    q1 = df[col].quantile(.25)
+    q3 = df[col].quantile(.75)
+    iqrscaled = iqr(df[col])*1.5
+    upper = df[df[col] > q3+ float(iqrscaled)]
+    lower = df[df[col] < float(iqrscaled) - q1]
+
+    if drop_top and drop_bot:
+        df = df.copy()
+        df.drop(upper.index,inplace = True)
+        return df.drop(lower.index)
+    elif drop_top:
+        return df.drop(upper.index)
+    elif drop_bot:
+        return df.drop(lower.index)
+
+    return pd.concat([upper[['parcelid',col]],lower[['parcelid',col]]])
+
+
+def scale_df(df,cols = []):
+    scaler = MinMaxScaler()
+    df = df.copy()
+    for col in cols:
+        scaler.fit(df[[col]])
+        df[col] = scaler.transform(df[[col]])
+    return df
 
 def only_singletons(df):
     return df.pipe(drop_multiunits)\
     .pipe(drop_no_bathbr)
 
+
+
 def prep_zillow(df):
-    return df.pipe(only_singletons)
+    return df.pipe(only_singletons)\
+    .pipe(drop_dup_parcelid)
